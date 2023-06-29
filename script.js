ca51f16e9d217158b2f7b907e9701c9563c11a54
supriyasingh711 <108217678+supriyasingh711@users.noreply.github.com>
2023-06-29 14:22:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const accessKey="lZRVjExEaiwScIUqQB3qFK8jZBPhZslIxLy3yQq_MlM"
+const formEl=document.querySelector("form")
+const inputEl=document.getElementById("search-input")
+const searchResults=document.querySelector(".search-results")
+const showMoreButton=document.getElementById("show-more-button")
+
+let inputData=""
+let page=1;
+ function searchImages(){
+    inputData=inputEl.value;
+    const url=
+ }
