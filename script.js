69525581b3e5457956a88bb7c6795119fe2bd47c
supriyasingh711 <108217678+supriyasingh711@users.noreply.github.com>
2023-06-29 16:15:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,39 @@ const showMoreButton=document.getElementById("show-more-button")
 
 let inputData=""
 let page=1;
- function searchImages(){
+async function searchImages(){
     inputData=inputEl.value;
-    const url=
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    const response=await fetch(url)
+    const data=await response.json()
+    const results=data.results
+    if (page===1){
+      searchResults.innerHTML=""
+    }
+    results.map((result)=>{
+      const imageWrapper=document.createElement("div")
+      imageWrapper.classList.add("search-results")
+      const image=document.createElement("img")
+      image.src=result.urls.small
+      image.alt=result.alt_description
+      const imageLink=document.createElement("a")
+      imageLink.href=result.links.html
+      imageLink.target="_blank"
+      imageLink.textContent=result.alt_description
+      imageWrapper.appendChild(image)
+      imageWrapper.appendChild(imageLink)
+     searchResults.appendChild(imageWrapper)
+    })
+    page++
+    if (page>1){
+      showMoreButton.style.display="block"
+    }
+formEl.addEventListener("submit",(event)=>{
+   event.preventDefault()
+   page=1
+   searchImages()
+})
+showMoreButton.addEventListener("click",()=>{
+  searchImages()
+})
  }
